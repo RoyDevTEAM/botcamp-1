b45d8e680bd8afcb3f2eee4102d0524a120b7bfe
RoyDevTEAM <roidercito12345@gmail.com>
2023-10-24 09:32:26 -0400

    hola

---
+++ script.js
@@ -0,0 +1,14 @@
+
+   // Testimonials carousel
+   $(".testimonials-carousel").owlCarousel({
+    center: true,
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+        0:{
+            items:1
+        }
+    }
+});
+
